9680b109ebcda0715dc824d75cd5c3f7810c9c04
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2026-05-24 19:53:12 -0300

    minor

--- project-design/src/pages/Home/page.tsx
+++ project-design/src/pages/Home/page.tsx
@@ -1,6 +1,6 @@
 export function Home() {
   return (
-    <div className="bg-surface p-4">
+    <div className="bg-on-surface p-4">
       <h1 className="text-3xl font-bold text-surface">Hello World!</h1>
     </div>
   );
